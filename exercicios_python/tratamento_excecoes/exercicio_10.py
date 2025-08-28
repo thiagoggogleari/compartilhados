614139a29bb35144d7ae11433f474ecabb37422a
thiago_work <thiagoggogleari@gmail.com>
2025-08-28 13:33:02 -0300

    resolvido exercicio 10 excecções

--- exercicios_python/tratamento_excecoes/exercicio_10.py
+++ exercicios_python/tratamento_excecoes/exercicio_10.py
@@ -10,15 +10,24 @@ O programa deve calcular a soma desses números.
 """
 
 def processar_arquivo_numeros(nome_arquivo):
-    # Escreva seu código aqui
-    pass
+    try: 
+        # Abrir o arquivo em modo de leitura
+        with open('C:/Users/n5687188/codigos/compartilhados/exercicios_python/tratamento_excecoes/numeros.txt', 'r', encoding='utf-8') as arquivo:
+            # Ler cada linha do arquivo
+            soma_numeros = 0 
+            for linha in arquivo:
+                # Remover quebras de linha e espaços extras
+                linha = linha.strip()
+                # Exibir a linha
+                print(linha)
+                try:
+                    soma_numeros = soma_numeros + int(linha)
+                except(ValueError):
+                    print("Caractere da linha do arquivo não é válido")
+        print("A soma dos números presentes no arquivo é de %i"%(soma_numeros))
+    except(FileNotFoundError):
+        print("Arquivo não encontrado")
+    finally:
+        print("Processamento de arquivo finalizado.")
+processar_arquivo_numeros("numeros.txt")
 
-# Para testar, crie um arquivo "numeros.txt" com alguns números e talvez algumas linhas de texto.
-# Exemplo de numeros.txt:
-# 10
-# 20.5
-# texto
-# 30
-#
-# Exemplo de uso:
-# processar_arquivo_numeros("numeros.txt")
