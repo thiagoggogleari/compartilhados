fef7090f0d061bda3e65b439546bc6c84f6871c4
Thiago <thiagoggogleari@gmail.com>
2025-09-30 21:50:30 -0300

    resolvido exercicios 11 e 12 django python

--- exercicios_python/django_python/exercicio_11.py
+++ exercicios_python/django_python/exercicio_11.py
@@ -33,5 +33,55 @@ A função deve:
 
 Se a tag não for encontrada, a função deve retornar uma lista vazia.
 """
+tags = [
+    {'id': 1, 'nome': 'Python'},
+    {'id': 2, 'nome': 'Django'},
+    {'id': 3, 'nome': 'WebDev'},
+]
 
-# Coloque sua solução abaixo
+posts = [
+    {'id': 1, 'titulo': 'Intro a Django', 'tags_ids': [2, 3]},
+    {'id': 2, 'titulo': 'Funções em Python', 'tags_ids': [1]},
+    {'id': 3, 'titulo': 'Desenvolvimento Web com Python', 'tags_ids': [1, 3]},
+]
+
+
+def filtrar_posts_por_tag(posts,tags,nome):
+    
+    nova_lista = []
+
+    # Busca o id do 'nome' recebido 
+    id_nome = ''
+    for k in range(len(tags)):
+#        print(tags[k]['nome'])
+        if tags[k]['nome'] == nome:
+           id_nome = tags[k]['id'] 
+           break
+    print("Nome: %s\nID: %s\n"%(nome,id_nome))
+
+    if id_nome =='':
+        return []
+
+    # Encontrar o 'id' da tag correspondente
+    for i in range(len(posts)):
+#        print(posts[i]['tags_ids'])
+        # Atribui as tags_id à uma lista
+        lista_tags = posts[i]['tags_ids']
+        
+        if id_nome in lista_tags:
+            nova_lista.append(posts[i])
+
+    # Retornar uma nova lista contendo apenas os posts que possuem o id em sua lista tags_id
+    return nova_lista 
+
+nova_lista = filtrar_posts_por_tag(posts,tags,'Python')
+print(nova_lista)
+
+
+
+
+
+
+
+
+
--- exercicios_python/django_python/exercicio_12.py
+++ exercicios_python/django_python/exercicio_12.py
@@ -30,5 +30,48 @@ O resultado esperado para o exemplo acima é:
 O método `.get(chave, valor_padrao)` de dicionários é muito útil aqui para 
 inicializar ou incrementar os contadores.
 """
+posts = [
+    {'id': 1, 'titulo': 'Post de Alice', 'autor_id': 101},
+    {'id': 2, 'titulo': 'Outro post de Alice', 'autor_id': 101},
+    {'id': 3, 'titulo': 'Post do Bob', 'autor_id': 102},
+    {'id': 4, 'titulo': 'Terceiro post de Alice', 'autor_id': 101},
+]
 
-# Coloque sua solução abaixo
+# Abordagem desenvolvida:
+
+def contar_posts_por_autor(posts):
+    # monta uma lista com os ids de autores cadastrados
+    lista_ids = []
+    for i in range(len(posts)):
+        if posts[i]['autor_id'] not in lista_ids:
+            lista_ids.append(posts[i]['autor_id'])
+    
+    # Objetivo , montar dicionário com ids e quantidades
+    dic_contagem ={}
+
+    # Percorre os posts contando as ocorrências
+    # Primeiro laço é para percorrer as ids em 'lista_ids'
+    for valor_procurado in lista_ids:
+        contador = 0
+        # Segundo laço é para contar as ocorrências
+        for id in posts:
+            if id['autor_id'] == valor_procurado:
+                contador += 1
+        # Atribui ao novo dicionário o id e quantidade encontrados
+        dic_contagem[valor_procurado] = contador
+        
+    return(dic_contagem)
+
+res = contar_posts_por_autor(posts)
+print(res)
+
+# Exemplo de abordagem sugerida:
+#contagem_por_autor = {}
+#
+#lista_autores = [101, 102, 101, 103, 102, 101]
+#
+#for autor_id in lista_autores:
+#    contagem_por_autor[autor_id] = contagem_por_autor.get(autor_id, 0) + 1
+#
+#print(contagem_por_autor)
+#
